141dfceedded532e8467f59eff80fe951eae5f3c
jltahara <jltahara@ucsc.edu>
2018-01-15 12:41:39 -0800

    Almost done

---
+++ Lab01/CE13_Lab1.X/part0.c
@@ -0,0 +1,17 @@
+/*
+ * File:   part0.c
+ * Author: JustinT
+ *
+ * Created on January 13, 2018, 1:21 PM
+ */
+
+
+#include "xc.h"
+#include "BOARD.h"
+
+void main(void)
+{
+    BOARD_Init();
+    printf("Hello World\n");
+    while (1);
+}
