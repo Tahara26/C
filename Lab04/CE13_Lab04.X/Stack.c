f033bd0d5b5075d7c5db30d5d747e3a2142d91d5
jltahara <jltahara@ucsc.edu>
2018-02-02 14:31:19 -0800

    Work in Progress Lab4

---
+++ Lab04/CE13_Lab04.X/Stack.c
@@ -0,0 +1,35 @@
+/*
+ * File:   Stack.c
+ * Author: JustinT
+ *
+ * Created on February 1, 2018, 9:45 AM
+ */
+
+
+#include "xc.h"
+#include "Stack.h"
+
+
+void StackInit(struct Stack *stack){
+    
+}
+
+int StackPush(struct Stack *stack, float value){
+    
+}
+
+int StackPop(struct Stack *stack, float *value){
+    
+}
+
+int StackIsEmpty(const struct Stack *stack){
+    
+}
+
+int StackIsFull(const struct Stack *stack){
+    
+}
+
+int StackGetSize(const struct Stack *stack){
+    
+}
