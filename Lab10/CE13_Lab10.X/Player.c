57c01bafdf12455897f956a025a182a0bca59ef5
jltahara <jltahara@ucsc.edu>
2018-03-16 13:24:54 -0700

    Starting Lab10

---
+++ Lab10/CE13_Lab10.X/Player.c
@@ -0,0 +1,51 @@
+/*
+ * File:   Player.c
+ * Author: JustinT
+ *
+ * Created on March 15, 2018, 9:20 PM
+ */
+
+#include "Game.h"
+#include "Player.h"
+#include "UnixBOARD.h"
+#include "xc.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+static int i;
+char inventory[INVENTORY_SIZE];
+
+/**
+ * Adds the specified item to the player's inventory if the inventory isn't full.
+ * @param item The item number to be stored: valid values are 0-255.
+ * @return SUCCESS if the item was added, STANDARD_ERRROR if the item couldn't be added.
+ */
+int AddToInventory(uint8_t item)
+{
+    for (i = 0; i < INVENTORY_SIZE; i++) {
+        inventory[i] = item;
+        return SUCCESS;
+    }
+    if (inventory == 0) {
+        return STANDARD_ERROR;
+    }
+}
+
+/**
+ * Check if the given item exists in the player's inventory.
+ * @param item The number of the item to be searched for: valid values are 0-255.
+ * @return SUCCESS if it was found or STANDARD_ERROR if it wasn't.
+ */
+int FindInInventory(uint8_t item)
+{
+    for (i = 0; i < INVENTORY_SIZE; i++) {
+        if (inventory[i] == item) {
+            return SUCCESS;
+        } else {
+            return STANDARD_ERROR;
+        }
+    }
+}
